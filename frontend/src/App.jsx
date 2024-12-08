c747979651cca0ec48364bbe3de263780c7dfb4f
WarnickAJA <javierq666@gmail.com>
2024-12-07 21:32:59 -0300

    testing db connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,13 @@ import Login from "./components/Auth0/Login";
 import Logout from "./components/Auth0/Logout";
 import Profile from "./components/Auth0/Profile";
 
+import useFetch from "./hooks/useFetch";
+
 function App() {
   const item = items[0];
   console.log(items);
+  const { data } = useFetch(import.meta.env.VITE_API_URL + "/api/courses");
+  console.log(data);
   return (
     <>
       <Login />
